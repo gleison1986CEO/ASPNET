7283f03ee7cc7a4c6d1b1eb422309cfed541ddc2
root <lionmonkeydesignapp@gmail.com>
2022-12-08 18:15:28 -0300

    // ASP MVC //

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using teste_.Models;
+
+
+namespace teste_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BancoDB _context;
+
+        public HomeController(BancoDB context)
+        {
+            _context = context;
+        }
+
+        // GET: Clientes
+
+
+
+        public async Task<IActionResult> Index(string Buscar_Clientes)
+        {
+            var cliente = from cr in _context.Cliente select cr;
+            if (!String.IsNullOrEmpty(Buscar_Clientes))
+            {
+                cliente = cliente.Where(c => c.Nome.Contains(Buscar_Clientes));
+            }
+            var bancoDB = _context.Cliente.Include(c => c.Cidade);
+            return View(cliente);
+        }
+
+       
+
+        // GET: Clientes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Cliente == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Cliente
+                .Include(c => c.Cidade)
+                .FirstOrDefaultAsync(m => m.Codigo == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // GET: Clientes/Create
+        public IActionResult Create()
+        {
+            ViewData["CidadeId"] = new SelectList(_context.Cidade, "Codigo", "Nome");
+            return View();
+        }
+
+        // POST: Clientes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Codigo,Nome,DataNascimento,CidadeId,Ativo")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CidadeId"] = new SelectList(_context.Cidade, "Codigo", "Nome", cliente.CidadeId);
+            return View(cliente);
+        }
+
+        // GET: Clientes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Cliente == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Cliente.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            ViewData["CidadeId"] = new SelectList(_context.Cidade, "Codigo", "Nome", cliente.CidadeId);
+            return View(cliente);
+        }
+
+        // POST: Clientes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Codigo,Nome,DataNascimento,CidadeId,Ativo")] Cliente cliente)
+        {
+            if (id != cliente.Codigo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cliente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClienteExists(cliente.Codigo))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CidadeId"] = new SelectList(_context.Cidade, "Codigo", "Nome", cliente.CidadeId);
+            return View(cliente);
+        }
+
+        // GET: Clientes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Cliente == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Cliente
+                .Include(c => c.Cidade)
+                .FirstOrDefaultAsync(m => m.Codigo == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Cliente == null)
+            {
+                return Problem("Entity set 'BancoDB.Cliente'  is null.");
+            }
+            var cliente = await _context.Cliente.FindAsync(id);
+            if (cliente != null)
+            {
+                _context.Cliente.Remove(cliente);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClienteExists(int id)
+        {
+            return _context.Cliente.Any(e => e.Codigo == id);
+        }
+    }
+    }
---
+++ Models/BancoDB.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using teste_.Models;
+
+namespace teste_.Models
+{
+    public class BancoDB : DbContext
+    {
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Cidade> Cidade { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            //ACESSO WEB //optionsBuilder.UseSqlServer(connectionString: @"Data Source = dbdev9.curitiba.pr.gov.br\dev2014; Initial Catalog = provacandidato; User ID = provacandidato; Password = desenvolvimento");
+            //ACESSO LOCALHOST
+            optionsBuilder.UseSqlServer(connectionString: "Data Source=DELL-1\\SQLEXPRESS;Initial Catalog = provacandidato; Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+    }
+}
